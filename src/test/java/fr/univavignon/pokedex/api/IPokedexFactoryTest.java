4b297371884a34439dbd420d0ba7242d51cb8b43
Mejai Wajdi <wajdi.mejai@alumni.univ-avignon.fr>
2024-10-18 15:51:10 +0200

    implementation de la classe IPokedexFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,4 +1,28 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 public class IPokedexFactoryTest {
+  private IPokedexFactory iPokedexFactory;
+  private IPokemonFactory iPokemonFactory;
+  private IPokemonMetadataProvider metadataProvider;
+  private IPokedex iPokedex;
+
+    @BeforeEach
+    public void setUp(){
+        iPokedexFactory= Mockito.mock(IPokedexFactory.class);
+        iPokemonFactory=Mockito.mock(IPokemonFactory.class);
+        metadataProvider=Mockito.mock(IPokemonMetadataProvider.class);
+
+    }
+    @Test
+    public void testCreatePokedex(){
+        when(iPokedexFactory.createPokedex(metadataProvider,iPokemonFactory)).thenReturn(iPokedex);
+        assertEquals(iPokedex,iPokedexFactory.createPokedex(metadataProvider,iPokemonFactory));
+    }
 }
